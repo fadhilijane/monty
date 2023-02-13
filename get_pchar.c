116cec851ca4d33e14763ccf1755cb30df11af7a
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-02-13 17:09:06 +0300

    Update get_pchar.c

--- get_pchar.c
+++ get_pchar.c
@@ -1,28 +1,29 @@
 #include "monty.h"
 
 /**
- * get_pchar - function that prints the char at the top of the stack
- * @stack: pointer to head of the stack
- * @line_number: where the instruction appears
- * Description: 11. pchar
- * Return: see below
- * 1. upon success, nothing
- * 2. upon fail, EXIT_FAILURE
- */
-void get_pchar(stack_t **stack, unsigned int line_number)
+* pchar - prints the char at the top of the stack
+* @stack: The stack list
+* @line_num: The line number of the file
+* Return: The head of the stack
+*/
+
+stack_t *pchar(stack_t **stack, unsigned int line_num)
 {
-if ((stack == NULL) || ((*stack) == NULL))
+stack_t *head = *stack;
+/* Check if stack is NULL */
+if (!head)
 {
-fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
-fclose(file);
-get_free(*stack);
+/* Print error, free, and exit */
+fprintf(stderr, "L%d: can't pchar, stack empty\n", line_num);
 exit(EXIT_FAILURE);
 }
-if (!(isascii((*stack)->n)))
+if (head->n < 0 || head->n > 127)
 {
-fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
-fclose(file);
+fprintf(stderr,
+"L%d: can't pchar, value out of range\n", line_num);
 exit(EXIT_FAILURE);
 }
-printf("%c\n", (*stack)->n);
+putchar(head->n);
+putchar('\n');
+return (*stack);
 }
