21fb488e0016f69b0c741f9448f2c59ee4384655
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-02-13 17:06:54 +0300

    Update get_mul.c

--- get_mul.c
+++ get_mul.c
@@ -1,31 +1,26 @@
 #include "monty.h"
 
 /**
- * get_mul - function that multiples the top two elements of the stack
- * @stack: pointer to the top of the stack
- * @line_number: where the line number appears
- * Description: 8. mul
- * Return: see below
- * 1. upon success, nothing
- * 2. upon fail, EXIT_FAILURE
- */
-void get_mul(stack_t **stack, unsigned int line_number)
-{
-stack_t *first, *second;
+* mul_op - multiplies the second top element of the stack with the top one
+* @stack: The stack list
+* @line_num: The line number of the file
+* Return: The edited stack
+*/
 
-if ((*stack == NULL) || ((*stack)->next == NULL))
+stack_t *mul_op(stack_t **stack, unsigned int line_num)
 {
-fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
-fclose(file);
+int result = 0;
+stack_t *head = *stack;
+/* Check if stack is NULL */
+if (!head || !head->next)
+{
+/* Print error, free, and exit */
+fprintf(stderr, "L%d: can't mul, stack too short\n", line_num);
+free_stack(stack);
 exit(EXIT_FAILURE);
 }
-else
-{
-first = *stack;
-second = first->next;
-second->n *= first->n;
-*stack = second;
-(*stack)->prev = NULL;
-free(first);
-}
+result = (head->next->n) * (head->n);
+head->next->n = result;
+pop(stack, 0);
+return (*stack);
 }
