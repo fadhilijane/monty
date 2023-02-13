7abf8ac82a3c6f401e8e18843c58a07d8e118df8
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-02-13 17:06:17 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,74 +1,68 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef _MONTY_H_
+#define _MONTY_H_
 
+/* Libraries */
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/stat.h>
 
-
-
+/* Declaration of the global variables */
+extern int token;
+/* Structures */
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
- */
+* struct stack_s - doubly linked list representation of a stack (or queue)
+* @n: integer
+* @prev: points to the previous element of the stack (or queue)
+* @next: points to the next element of the stack (or queue)
+* Description: doubly linked list node structure
+* for stack, queues, LIFO, FIFO Holberton project
+*/
+
 typedef struct stack_s
 {
-  int n;
-  struct stack_s *prev;
-  struct stack_s *next;
+int n;
+struct stack_s *prev;
+struct stack_s *next;
 } stack_t;
 
-
-
 /**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
- */
+* struct instruction_s - opcode and its function
+* @opcode: the opcode
+* @f: function to handle the opcode
+* Description: opcode and its function
+* for stack, queues, LIFO, FIFO Holberton project
+*/
+
 typedef struct instruction_s
 {
-  char *opcode;
-  void (*f)(stack_t **stack, unsigned int line_number);
+char *opcode;
+stack_t *(*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/* Prototypes */
 
-extern FILE *file;
-FILE *file;
+stack_t *push(stack_t **stack, unsigned int line_number);
+stack_t *pall(stack_t **stack, unsigned int line_number);
+stack_t *pint(stack_t **stack, unsigned int line_number);
+stack_t *pop(stack_t **stack, unsigned int line_number);
+stack_t *swap(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t **stack);
+stack_t *(*op_func(char *s, stack_t **stack,
+unsigned int line_number))(stack_t **stack,
+unsigned int line_number);
+stack_t *add(stack_t **stack, unsigned int line_number);
+stack_t *sub(stack_t **stack, unsigned int line_number);
+stack_t *nop(stack_t **stack, unsigned int line_number);
+stack_t *mod(stack_t **stack, unsigned int line_number);
+stack_t *div_op(stack_t **stack, unsigned int line_number);
+stack_t *mul_op(stack_t **stack, unsigned int line_num);
+stack_t *pchar(stack_t **stack, unsigned int line_num);
+stack_t *pstr(stack_t **stack, unsigned int line_number);
+stack_t *rotl(stack_t **stack, unsigned int line_number);
 
-void (*operator_function)(stack_t **, unsigned int);
-void (*go(char *op_f, unsigned int l, stack_t **s))(stack_t**, unsigned int);
-
-
-void get_push(stack_t **stack, unsigned int line_number, char *temp);
-void get_pall(stack_t **stack, unsigned int line_number);
-void get_pint(stack_t **stack, unsigned int line_number);
-void get_pop(stack_t **stack, unsigned int line_number);
-void get_swap(stack_t **stack, unsigned int line_number);
-void get_add(stack_t **stack, unsigned int line_number);
-void get_nop(stack_t **stack, unsigned int line_number);
-void get_sub(stack_t **stack, unsigned int line_number);
-void get_div(stack_t **stack, unsigned int line_number);
-void get_mul(stack_t **stack, unsigned int line_number);
-void get_mod(stack_t **stack, unsigned int line_number);
-void get_rotl(stack_t **stack, unsigned int line_number);
-void get_pchar(stack_t **stack, unsigned int line_number);
-void get_rotr(stack_t **stack, unsigned int line_number);
-void get_pstr(stack_t **stack, unsigned int line_number);
-
-void get_free(stack_t *stack);
-int _isdigit(char *str);
-
-
-#endif /* MONTY_H */
+#endif /* _MONTY_H_ */
