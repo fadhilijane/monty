2e75060f51a8c5635a8dd17cdde1ac02a88e27c0
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-02-13 17:13:54 +0300

    Update get_swap.c

--- get_swap.c
+++ get_swap.c
@@ -1,37 +1,30 @@
 #include "monty.h"
 
 /**
- * get_swap - function that swaps the top two elements on the stack
- * @stack: pointer to the top of the stack
- * @line_number: where the line number appears
- * Description: 3. swap
- * Return: see below
- * 1. upon success, nothing
- * 2. upon fail, EXIT_FAILURE
- */
-void get_swap(stack_t **stack, unsigned int line_number)
-{
-stack_t *first, *second;
+* swap - swaps the top two elements of the stack
+* @stack: ponter to the list stack
+* @line_number: unused variable
+* Return: the address of the first node
+*/
 
-if ((*stack == NULL) || ((*stack)->next == NULL))
+stack_t *swap(stack_t **stack,
+unsigned int line_number __attribute__((unused)))
 {
-fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-fclose(file);
-get_free(*stack);
+stack_t *head;
+int tmp;
+head = *stack;
+if (head != NULL && head->next != NULL)
+{
+tmp = head->n;
+head->n = head->next->n;
+head->next->n = tmp;
+}
+else
+{
+fprintf(stderr, "L%d: can't swap, stack too short\n",
+line_number);
+free_stack(stack);
 exit(EXIT_FAILURE);
 }
-else if (*stack)
-{
-first = *stack;
-second = first->next;
-*stack = second;
-first->next = second->next;
-if (second->next != NULL)
-{
-second->next->prev = first;
-}
-second->prev = NULL;
-second->next = first;
-first->prev = second;
-}
+return (*stack);
 }
