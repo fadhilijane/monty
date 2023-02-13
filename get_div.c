bb7f10454b3974dc39ee28c126fe98a029779e3f
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-02-13 17:02:01 +0300

    Update get_div.c

--- get_div.c
+++ get_div.c
@@ -1,38 +1,35 @@
 #include "monty.h"
 
 /**
- * get_div - function that divides the top two elements of the stack
- * @stack: pointer to the top of the stack
- * @line_number: where the line number appears
- * Description: 7. div
- * Return: see below
- * 1. upon success, nothing
- * 2. upon fail, EXIT_FAILURE
- */
-void get_div(stack_t **stack, unsigned int line_number)
-{
-stack_t *first, *second;
+* div_op - This function divides the second top nodes of the stack by the top
+* @stack: The stack list
+* @line_num: The line number of the file
+* Return: The edited stack
+*/
 
-if ((*stack == NULL) || ((*stack)->next == NULL))
+stack_t *div_op(stack_t **stack, unsigned int line_num)
 {
-fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
-fclose(file);
-free(*stack);
+int result = 0;
+stack_t *head = *stack;
+/* Check if stack is NULL */
+if (!head || !head->next)
+{
+/* Print error, free, and exit */
+fprintf(stderr, "L%d: can't div, stack too short\n", line_num);
+free_stack(stack);
 exit(EXIT_FAILURE);
 }
-else if ((*stack)->n == 0)
+if (head->n == 0)
 {
-fprintf(stderr, "L%d: division by zero\n", line_number);
-fclose(file);
-get_free(*stack);
+fprintf(stderr, "L%d: division by zero\n", line_num);
 exit(EXIT_FAILURE);
 }
-else
-{
-first = *stack;
-second = first->next;
-second->n /= first->n;
-*stack = second;
+/* Div second node by the first one and strore result in first node */
+result = (head->next->n) / (head->n);
+head->next->n = result;
+pop(stack, 0);
+return (*stack);
+}
 (*stack)->prev = NULL;
 free(first);
 }
