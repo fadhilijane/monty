fffccfa5f02d0760524661efd194acc9acdd72be
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-02-13 17:04:42 +0300

    Update get_mod.c

--- get_mod.c
+++ get_mod.c
@@ -1,39 +1,32 @@
 #include "monty.h"
 
 /**
- * get_mod - function that finds the modulus of top two elements
- * @stack: pointer to the top of the stack
- * @line_number: where the line number appears
- * Description: 9. mod
- * Return: see below
- * 1. upon success, nothing
- * 2. upon fail, EXIT_FAILURE
- */
-void get_mod(stack_t **stack, unsigned int line_number)
-{
-stack_t *first, *second;
+* mod - This function computes the rest of the division of the
+* second top element of the stack by the top element of the stack
+* @stack: The pointer to the stack
+* @line_number: The current line number in the file
+* Return: The pointer to the stack
+*/
 
-if ((*stack == NULL) || ((*stack)->next == NULL))
+stack_t *mod(stack_t **stack, unsigned int line_number)
 {
-fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
-fclose(file);
-get_free(*stack);
+stack_t *head = *stack;
+int mod_int = 0;
+if (!head || !(head->next))
+{
+fprintf(stderr, "L%d: can't mod, stack too short\n",
+line_number);
+free_stack(stack);
 exit(EXIT_FAILURE);
 }
-else if ((*stack)->n == 0)
+if (head->n == 0)
 {
 fprintf(stderr, "L%d: division by zero\n", line_number);
-fclose(file);
-get_free(*stack);
+free_stack(stack);
 exit(EXIT_FAILURE);
 }
-else
-{
-first = *stack;
-second = first->next;
-second->n %= first->n;
-*stack = second;
-(*stack)->prev = NULL;
-free(first);
-}
+mod_int = (head->next->n) % (head->n);
+head->next->n = mod_int;
+pop(stack, 0);
+return (*stack);
 }
